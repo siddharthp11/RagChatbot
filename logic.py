a280948dddaed96c654b9c758ff74f8ead34de69
Sid <siddharthpillai123@gmail.com>
2023-11-15 11:16:16 -0500

    feat: api key entry box

--- logic.py
+++ logic.py
@@ -17,36 +17,46 @@ class Retrieval:
     
     def get_query_embedding(self, text, model ="text-search-curie-query-001"):
         try:
-            print(text)
             result = ai.Embedding.create(model=model, input=text)
-            print("Embedded query!")
             return result["data"][0]["embedding"]
 
         except Exception as e:
-            print("Error while eEmbedding: ", e)
+            assert Exception("Could not embed the query.")
 
 
     # Find relevant chunks for a query.
     def order_document_sections_by_query_similarity(self, query):
-        query_embedding = self.get_query_embedding(query)
-        document_similarities = sorted([
-            (self.vector_similarity(query_embedding, doc_embedding), doc_index) for doc_index, doc_embedding in self.document_embeddings.items()
-        ], reverse=True)
+        try:
+            query_embedding = self.get_query_embedding(query)
+            document_similarities = sorted([
+                (self.vector_similarity(query_embedding, doc_embedding), doc_index) for doc_index, doc_embedding in self.document_embeddings.items()
+            ], reverse=True)
 
-        return document_similarities
+            return document_similarities
+        except Exception as e: 
+            return e 
 
     def get_context(self, question):
-        info = self.order_document_sections_by_query_similarity(question)[:5]
-        contexts = [self.documents[int(chunk[1])] for chunk in info ]
+        try: 
+            info = self.order_document_sections_by_query_similarity(question)[:5]
+            contexts = [self.documents[int(chunk[1])] for chunk in info ]
 
-        return contexts
+            return contexts
+        except Exception as e: 
+            return []
 
 class ChatBot:
-    def __init__(self, api_key,  model='curie'):
-        print(ai.api_key)
-        ai.api_key = api_key
+    def __init__(self,  model='curie'):
         self.model = model
         self.retrieval = Retrieval()
+
+    def can_initialize(): 
+        try:
+            ai.Model.list()
+        except ai.error.AuthenticationError as e:
+            return False
+        else:
+            return True
 
     def get_response(self, question):
         contexts = self.retrieval.get_context(question)
@@ -58,15 +68,19 @@ class ChatBot:
 
         Q: {question}
         A:""".format(ctx=context_as_string, question=question)
-        response = ai.Completion.create(
-            prompt=prompt,
-            temperature=0,
-            max_tokens=300,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            model="text-davinci-002"
-        )["choices"][0]["text"].strip(" \n")
 
-        return contexts, response
+        try: 
+            response = ai.Completion.create(
+                prompt=prompt,
+                temperature=0,
+                max_tokens=300,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                model="text-davinci-002"
+            )["choices"][0]["text"].strip(" \n")
+
+            return contexts, response
+        except Exception as e: 
+            return 'No context found', 'No response'
 
--- ui.py
+++ ui.py
@@ -1,23 +1,31 @@
 from logic import ChatBot
 import streamlit as st 
-
-if 'chatbot' not in st.session_state:
-    st.session_state.chatbot = ChatBot("")
+import openai as ai 
 
 
-chatbot =  st.session_state['chatbot']
+if not ChatBot.can_initialize():
+    st.title('Retrieval Augmented Generation ChatBot!')
+    st.info('This chatbot has access to information about internet marketing.')
 
-st.title('Retrieval Augmented Generation ChatBot!')
-st.info('This chatbot has access to information about internet marketing.')
+    api_key = st.text_input('Enter your API Key')
+    ai.api_key = api_key 
 
-input = st.text_input("Enter your question:")
-contexts, response = '', ''
+else: 
+    st.title('Retrieval Augmented Generation ChatBot!')
+    st.info('This chatbot has access to information about internet marketing.')
+    
+    if 'chatbot' not in st.session_state: 
+        st.session_state['chatbot'] = ChatBot()
 
-if input: 
-    contexts, response = chatbot.get_response(input)
+    chatbot =  st.session_state['chatbot']
+    input = st.text_input("Enter your question:")
+    contexts, response = '', ''
 
-st.text_area('Model response:', response)
+    if input: 
+        contexts, response = chatbot.get_response(input)
 
-if contexts:
-    for c, ctx in enumerate(contexts): 
-        st.text_area(f"Retrieved context #{c}", ctx)
+    st.text_area('Model response:', response)
+
+    if contexts:
+        for c, ctx in enumerate(contexts): 
+            st.text_area(f"Retrieved context #{c}", ctx)
